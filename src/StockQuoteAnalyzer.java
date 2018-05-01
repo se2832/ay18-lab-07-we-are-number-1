d678fcaa22ffaa1c34650db0ca0880d4b6fc787c
lindsleyae <lindsleyae@msoe.edu>
2018-05-01 17:52:38 -0500

    Fix Issue #2 where incorrect exception on line 97 is thrown

--- src/StockQuoteAnalyzer.java
+++ src/StockQuoteAnalyzer.java
@@ -94,7 +94,7 @@ public class StockQuoteAnalyzer {
 			throw new InvalidStockSymbolException("Symbol " + symbol + "not found.");		//Issue #1
 		}
 		if (stockQuoteSource == null) {
-			throw new InvalidStockSymbolException("The source for stock quotes can not be null");
+			throw new NullPointerException("The source for stock quotes can not be null");       //Issue #2
 		}
 		this.stockQuoteSource = stockQuoteSource;
 		this.audioPlayer = audioPlayer;
